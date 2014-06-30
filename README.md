df20337e31cc90adcd5ef626164b550e3a6102b6
Robert Kowalski <rok@kowalski.gd>
2014-06-30 15:23:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# require-benchmark
+
+Benchmarks to test a new require feature
+
+## Preparation
+
+```
+$ create-data.sh
+```
+
+Will create the testdata. It will also download a node webserver for tests
+on a real world example.
+
+## My Testplan
+
+1. reboot
+2. ./run-benchmark hapi
+
+3. reboot
+4. ./run-benchmark flat
+
+5. reboot
+6. ./run-benchmark nested
+
+## My Results
+
+### hapi
+
+### flat
+
+### nested
---
+++ create-data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "creating data. this may take some time."
+
+echo "console.time('node')" >> flat.js
+mkdir -p data
+for i in {0..10000};
+do
+	echo '{"id": "$i"}' > data/$i.json
+	echo "require('./data/$i')" >> flat.js
+	mkdir data/$i
+	echo '{"id": $i}' > data/$i/$i.json
+done
+echo "console.timeEnd('node')" >> flat.js
+
+
+DIR="data/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/"
+mkdir -p $DIR
+
+echo "console.time('node')" >> $DIR/nested.js
+echo '{"id": "$i"}' > nested-testfile.json
+echo "require('./../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../nested-testfile')" >> $DIR/nested.js
+echo "console.timeEnd('node')" >> $DIR/nested.js
+
+
+git clone https://github.com/npm/newww
+cd newww && git reset --hard 3c6fd1ae706ac9fd6a48e97103d6867a02a3f8e6
+npm install
+node allPlugins.js install
+echo "console.time('node')" | cat - server.js > /tmp/out && mv /tmp/out server.js
+sed 's/log.info(.Hapi server started @ . + server.info.uri);/console.timeEnd("node");process.exit();/g' server.js > /tmp/out && mv /tmp/out server.js
+
+echo "finished."
